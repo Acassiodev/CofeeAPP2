fefae5798b8f0854ff747095205f7c9041846765
Acássio Soares <112977988+Acassiodev@users.noreply.github.com>
2023-06-06 18:10:43 -0300

    first commit

---
+++ app/config/categories.js
@@ -0,0 +1,18 @@
+// list of coffee categories
+const categories = [
+  {
+    id: 1,
+    name: "Cappuccino",
+  },
+  {
+    id: 2,
+    name: "Expresso",
+  },
+  { id: 3, name: "Café com Leite" },
+  {
+    id: 4,
+    name: "Cafes a moda da Casa",
+  },
+];
+
+export default categories;
---
+++ app/config/coffees.js
@@ -0,0 +1,93 @@
+// list of coffes
+export default [
+  {
+    id: 1,
+    name: "Cappuccino",
+    image: require("../../assets/coffees/nathan-dumlao-c2Y16tC3yO8-unsplash.jpg"),
+    price: "6.00",
+    description:
+      "Cappuccino é uma bebida de café feita com café expresso e leite quente. É tradicionalmente preparado com leite vaporizado e tradicionalmente coberto com uma pequena quantidade de espuma.",
+    categoryId: 1,
+    rating: 4.5,
+    included: "Com Leite de Aveia",
+  },
+  {
+    id: 2,
+    name: "Expresso",
+    image: require("../../assets/coffees/nathan-dumlao-ikU3J1nr52w-unsplash.jpg"),
+    price: "4.01",
+    description:
+      "Espresso é uma bebida de café feita com grãos de café quentes que são torrados e depois preparados com água quente, resultando em um forte, encorpado e concentrado típico de um cappuccino. É tradicionalmente preparado com leite vaporizado e tradicionalmente coberto com uma pequena quantidade de espuma.",
+    categoryId: 1,
+    rating: 4.5,
+    included: "Com Leite de Aveia"
+    
+  },
+  {
+    id: 3,
+    name: "Café Preto",
+    image: require("../../assets/coffees/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg"),
+    price: "3.05",
+    description:
+    "Latte é uma bebida de café feita com café expresso e leite vaporizado. É tradicionalmente preparado com leite vaporizado e tradicionalmente é coberto com uma pequena quantidade de espuma.",
+    categoryId: 4,
+    rating: 4.5,
+    included: "Com Leite de Aveia"
+    
+  },
+  {
+    id: 4,
+    name: "Café a moda da casa",
+    image: require("../../assets/coffees/robbie-down-LI8inyHnm_A-unsplash.jpg"),
+    price: "3.00",
+    description:
+    "Café a moda da casa  é uma bebida de café feita com café expresso e leite vaporizado. É tradicionalmente preparado com leite vaporizado e tradicionalmente é coberto com uma pequena quantidade de espuma.",
+    categoryId: 4,
+    rating: 4.5,
+    included: "Com Leite de Aveia",
+  },
+  {
+    id: 5,
+    name: "Macchiato",
+    image: require("../../assets/coffees/fahmi-fakhrudin-nzyzAUsbV0M-unsplash.jpg"),
+    price: "4.00",
+    description:
+    "Macchiato é uma bebida de café feita com café expresso e leite quente. Tradicionalmente é preparado com leite vaporizado, sendo tradicionalmente coberto com uma pequena quantidade de espuma.",
+    categoryId: 2,
+    rating: 4.5,
+    included: "Com Leite de Aveia",
+  },
+  {
+    id: 6,
+    name: "Mocha",
+    image: require("../../assets/coffees/taylor-franz-GJogrGZxKJE-unsplash.jpg"),
+    price: "5.00",
+    description:
+      "Mocha é uma bebida de café feita com grãos de café quentes que são torrados e depois coados com água quente, resultando em um Mocha forte, encorpado e concentrado, típico. É tradicionalmente preparado com leite vaporizado e tradicionalmente coberto com uma pequena quantidade de espuma.",
+    categoryId: 2,
+    rating: 4.5,
+    included: "Com Leite de Aveia",
+  },
+  {
+    id: 7,
+    name: "Coretto",
+    image: require("../../assets/coffees/fahmi-fakhrudin-nzyzAUsbV0M-unsplash.jpg"),
+    price: "2.07",
+    description:
+    "Coretto é uma bebida de café feita com café expresso e leite quente. Tradicionalmente é preparado com leite vaporizado, sendo tradicionalmente coberto com uma pequena quantidade de espuma.",
+    categoryId: 3,
+    rating: 4.5,
+    included: "Com Leite de Aveia",
+  },
+  {
+    id: 8,
+    name: "Ristretto",
+    image: require("../../assets/coffees/taylor-franz-GJogrGZxKJE-unsplash.jpg"),
+    price: "1.99",
+    description:
+    "Ristretto é uma bebida de café feita com grãos de café quentes que são torrados e depois coados com água quente, resultando em um cappuccino forte, encorpado e concentrado, típico. É tradicionalmente preparado com leite vaporizado e tradicionalmente coberto com uma pequena quantidade de espuma.",
+    categoryId: 3,
+    rating: 4.5,
+    included: "Com Leite de Aveia",
+  },
+];
